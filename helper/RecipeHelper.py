b36720ce4627477543925f0ebe52782ab901cf70
luciobattisti <battistilucio194@gmail.com>
2019-01-27 18:16:05 -0800

    Fix add recipe ingredient bug

--- helper/RecipeHelper.py
+++ helper/RecipeHelper.py
@@ -415,9 +415,21 @@ class RecipeHelper:
         RecipeHelper.grid_ingredient_row(ingredient_row)
 
     def add_recipe_ingredients(self, w, recipe_name, curr_ingredients):
+        """
+        Add recipe ingredients to widget.
+
+        :param w: Widget root
+        :rtype: ``tkinter.Tk``
+        :param recipe_name: Recipe name
+        :type: ``str``
+        :param curr_ingredients: Stack of ingredients
+        :rtype: ``list``
+        :return:
+        """
 
         # Empty curr_ingredients stack
-        for row_ingredient in curr_ingredients:
+        num_ingredients = len(curr_ingredients)
+        for i in range(num_ingredients):
             RecipeHelper.remove_ingredient_row(curr_ingredients, min_threshold=0)
 
         # Get recipe ingredients and init row_id
