fbf419ec83d39aeb964e8881fd3873a3254795d2
luciobattisti <45297594+luciobattisti@users.noreply.github.com>
2022-10-28 19:00:52 -0400

    Add files via upload
    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="recipe_lib",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+       "pint==0.8.1"
+    ]
+
+)
+
